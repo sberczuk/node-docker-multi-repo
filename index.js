752fe02431bee16f2f83ef0cc28fd2cac800f250
sberczuk <steve@berczuk.com>
2018-05-18 14:57:59 -0400

    Make dirs relative to docker compose file. Add option to report

--- index.js
+++ index.js
@@ -6,19 +6,30 @@ const exec = util.promisify(require('child_process').exec);
 
 const yaml = require('js-yaml');
 const fs = require('fs');
-// Look into how to package this as a command line (Node JS In Action book?)
-
+const path = require('path');
 const program = require('commander');
 
 program
   .version(0.1)
-  .option('-d, --dockerFile <dockerFile>', 'Docker Compose file to read')
+  .option('-d, --dockerFile <dockerFile>', 'Docker Compose file to read (Required)')
+  .option('-b, --branch <branch>', 'Branch to check(Required)')
   .option('-s --statusOnly', 'only report on what branches are not on dev')
   .parse(process.argv);
 
 console.log(`Using Docker ${program.dockerFile}`);
 const dockerComposeFile = program.dockerFile;
 const doChange = !program.statusOnly;
+const defaultBranch = program.branch;
+
+if(!defaultBranch || ! dockerComposeFile){
+  program.outputHelp();
+  return;
+}
+
+// Assume that the docker compose is in a peer folder of the rest of the code
+// we can add an override later
+const codeBaseDir = path.normalize(path.dirname(path.dirname(dockerComposeFile)));
+
 
 async function execFunc(cmd, dir, outputProcessor) {
   const { stdout, stderr } = await exec('git status', { cwd: dir });
@@ -29,17 +40,16 @@ async function execFunc(cmd, dir, outputProcessor) {
 
 async function gitStatus(dir, command) {
   let nonDevDirs = [];
-  console.log(`CWD is ${dir}`);
   const branch = await execFunc('git status', dir, (a, b) => {
     const match = a.match('On branch (.+)');
     //console.log(`The Callback ${match}`);
     return match[1]; // 0 is the whole string
   });
-  if (branch != 'development') {
-    console.log(` ${dir} is ${branch} not development`);
+  if (branch != defaultBranch) {
+    console.log(` ${dir} is ${branch} not ${defaultBranch}`);
     nonDevDirs.push(dir);
     if(doChange){
-      console.log(`Changing ${dir} ${branch} -> development`);
+      console.log(`Changing ${dir} ${branch} -> ${defaultBranch}`);
       const { stdout2, stderr2 } = exec('git checkout development', dir);
     }
   }
@@ -69,8 +79,8 @@ try {
       console.log(volumes.filter(v => v.startsWith('../ts-')));
       const projectDirs = volumes.filter(v => v.startsWith('../ts-'));
       projectDirs.forEach(f => {
-        const baseDir = f.split(':')[0].replace('../', '');
-        const projectPath = `/Users/sberczuk/code/${baseDir}`;
+        const projDir = f.split(':')[0].replace('../', '');
+        const projectPath = `${codeBaseDir}/${projDir}`;
         console.log(`>>>> ${projectPath}`);
 
         // collect all that are not on development and can't be switched
