c80c7ec7679bff24bdbad6b5bb2c06ae5fea3a8d
sberczuk <steve@berczuk.com>
2018-05-18 14:22:02 -0400

    Used callbacks and Async better

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-
-'use strict'
+'use strict';
 
 const util = require('util');
 //const execSync = require('child_process').execSync;
 const exec = util.promisify(require('child_process').exec);
 
-
 const yaml = require('js-yaml');
-const fs   = require('fs');
+const fs = require('fs');
 // Look into how to package this as a command line (Node JS In Action book?)
 
 const program = require('commander');
@@ -17,53 +15,38 @@ program
   .option('-d, --dockerFile <dockerFile>', 'Docker Compose file to read')
   .parse(process.argv);
 
+console.log(`Using Docker ${program.dockerFile}`);
+const dockerComposeFile = program.dockerFile;
 
-  console.log(`Using Docker ${program.dockerFile}`);
- const dockerComposeFile = program.dockerFile;
-
- async function execFunc (cmd, dir, outputProcessor) {
-  const { stdout, stderr } =  exec('git status', {cwd: dir});
-  console.log(stdout);
-  return outputProcessor(stdout, stderr);
+async function execFunc(cmd, dir, outputProcessor) {
+  const { stdout, stderr } = await exec('git status', { cwd: dir });
+  if (outputProcessor) {
+    return outputProcessor(stdout, stderr);
+  }
 }
 
 async function gitStatus(dir, command) {
   let nonDevDirs = [];
   console.log(`CWD is ${dir}`);
-  /**
-  const branch =   await execFunc('git status',dir, (a,b) => {
-   const match = a.match('On branch (.+)');
-  return;
-    match[0];
-
+  const branch = await execFunc('git status', dir, (a, b) => {
+    const match = a.match('On branch (.+)');
+    //console.log(`The Callback ${match}`);
+    return match[1]; // 0 is the whole string
   });
-  */
-  const { stdout, stderr } =  await exec('git status', {cwd: dir});
-  const match = stdout.match('On branch (.+)');
-  const branch = match[0];
-  console.log (`branch is ${branch}`);
-  return;
-  //const { stdout, stderr } =  exec('git status', {cwd: dir});
-  //const match = stdout.match('On branch (.+)');
-  //const branch = match[0];
-  if(branch != 'development'){
-    nonDevDirs.push(dir)
+  console.log(`THE BRANCH ${branch}`);
+  if (branch != 'development') {
+    nonDevDirs.push(dir);
     console.log(`Changing ${dir} ${branch} -> development`);
-
-    const { stdout2, stderr2 } =  exec('git checkout development', {cwd: dir});
-
+    const { stdout2, stderr2 } = exec('git checkout development', dir);
   }
-  // always
   console.log(`updating ${dir}`);
-  const { stdout3, stderr3 } =  exec('git pull', {cwd: dir});
+  await execFunc('git pull', dir);
   console.log(`yarn install ${dir}`);
-
-  const { stdout4, stderr4 } =  exec('yarn install', {cwd: dir});
+  await execFunc('yarn install', dir);
 
   //console.log('stdout:', stdout);
   //console.log('stderr:', stderr);
   console.log(`dirs not on dev ${nonDevDirs}`);
-
 }
 
 // Get document, or throw exception on error
@@ -74,27 +57,26 @@ try {
   console.log('docker deps include:');
   console.log(Object.keys(doc.services));
   //console.log(Object.keys(doc.services['file']));
-  for(var service in doc.services){
+  for (var service in doc.services) {
     console.log(service);
     const s = doc.services[service];
-    if(s.hasOwnProperty('volumes')){
+    if (s.hasOwnProperty('volumes')) {
       const volumes = s['volumes'];
-      console.log(volumes.filter( v => v.startsWith('../ts-')));
-      const projectDirs = volumes.filter( v => v.startsWith('../ts-'));
+      console.log(volumes.filter(v => v.startsWith('../ts-')));
+      const projectDirs = volumes.filter(v => v.startsWith('../ts-'));
       projectDirs.forEach(f => {
-       const baseDir = f.split(':')[0].replace('../', '');
-       const projectPath =`/Users/sberczuk/code/${baseDir}`;
-       console.log(`>>>> ${projectPath}`);
+        const baseDir = f.split(':')[0].replace('../', '');
+        const projectPath = `/Users/sberczuk/code/${baseDir}`;
+        console.log(`>>>> ${projectPath}`);
 
-       // collect all that are not on development and can't be switched
-      gitStatus(projectPath)
-    });
+        // collect all that are not on development and can't be switched
+        gitStatus(projectPath);
+      });
     }
-
   }
- //console.log();
+  //console.log();
 
-// console.log(doc);
+  // console.log(doc);
 
   //console.log(doc);
 } catch (e) {
